2879bf139ecfe40af374e715e6c68a617e1ee591
Pedro <Pedrosilva704@hotmail.com>
2018-12-20 21:54:03 +0000

    Cores e alteraçoes
    Cores e alteraçoes a structs

--- comboios.h
+++ comboios.h
@@ -3,53 +3,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "cores.h"
-// defines para cores// sugiro passar isto para um ficheiro cores.h
-/*
-#define R  0
-#define G  1
-#define B  2
-*/
-#define ALFA  4//???
-#define AMARELO 0
-#define VERMELHO 1
-#define AZUL 2
-#define DIMCores 3 // para a matriz
-#define DIMrgb 4 // para a matriz
+#include "cores.h"
 //-------------------
 #define MAX 100
 /*--------------------------------------------------------------------------------*/
-typedef struct pontos {
-	int nOrdem;
+typedef struct linha {
+	//int nOrdem;
 	char ident[5];
-	int coord[2];
+	int coord[2];// em pixeis
 	int cor; // cor=VERMELHO
-	char tipo[4];//VIA, EST, ponto de via nao aparece ID no boneco
-	int raio;
-	int nEntradas;
-	int nSaidas;
-}PONTOS;
+	char TipoDePonto[4];//VIA, EST, ponto de via nao aparece ID no boneco
+	int Dimensao; //dimensao do ponto em píxeis
+	int nEntradas;//max 2
+	int nSaidas;//max 2
+
+}FERROVIA;
 
 typedef struct linha {
-	char ident[4];
 	PONTOS p;
 	struct linha *prox;
 }LINHA;
 
-typedef struct carr {
-	int cor[9];//usar rand
+/*typedef struct carr {
+	int cor[9];//usar rand ?????
 	int raio;//em pixeis
-	int posicao; // para verificar colisoes
+	int posicao[2]; // para verificar colisoes
 }CARRUAGEM;
-
+*/
 typedef struct comboio{
-	CARRUAGEM c; //
-	char ident[4];
-	int nCarruagens[4];//numero de carruagens
-	//int raio;// raio das bolas => CARRUAGEM.raio
-	char PosInicial[6];// ID da linha + ID do ponto
-	int cor;/* cor do comboio é cor da locomotiva, primeira CARRUAGEM*/
-	struct comboio *prox;
+	char ident[3];//BA
+	int nCarruagens[4];//numero de carruagens( dimensao)
+	int DimBOLAS; // raio em pixeis
+	int cor; /* cor do comboio é cor da locomotiva, primeira CARRUAGEM*/
+	char PosInicial[6];	/*ex: *LinhaAB->ident e LinhaAB->p->ident ??
+    ID da linha + ID do ponto */
+    int PosiNoGraf[2];//posição atual da carruagem\locomotiva
+
+	struct comboio *prox; // aponta para a prox carruagem
 }COMBOIO;
 
 void MostraInfoFerrovia(LINHA* queLinha);
---
+++ cores.h
@@ -0,0 +1,18 @@
+#define R  0
+#define G  1
+#define B  2
+#define ALFA  3
+
+#define VERMELHO 0
+#define AZUL 1
+#define AMARELO 2
+#define CYAN 3
+#define ROXO 4
+#define VERDE 5
+#define CASTANHO 6
+#define PRETO 7
+#define CINZENTO 8
+#define BRANCO 9
+
+#define DIMCores 10 // para a matriz
+#define DIMrgb 4 // para a matriz
