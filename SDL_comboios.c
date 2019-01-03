8234c997cd6e5bd4c0b239350981a4ad501dc6e6
Pedro <Pedrosilva704@hotmail.com>
2019-01-03 16:15:58 +0000

    k

--- SDL_comboios.c
+++ SDL_comboios.c
@@ -91,7 +91,7 @@ void SDL_escreve_linhas(FERROVIA *todas[],int cores[][DIMrgb]){
 		    printf("Correu escreve linhas FOR %d \n",i);
 
 			/*SDL_RenderDrawPoint(g_pRenderer, head->pont.coord[coordX], head->pont.coord[coordY];*/
-			filledCircleRGBA(g_pRenderer, head->pont.coord[coordX], head->pont.coord[coordY], head->pont.Dimensao, cores[head->pont.cor][R], cores[head->pont.cor][G], cores[head->pont.cor][B], cores[head->pont.cor][ALPA]);
+			filledCircleRGBA(g_pRenderer, head->pont.coord[coordX], head->pont.coord[coordY], 5, cores[head->pont.cor][R], cores[head->pont.cor][G], cores[head->pont.cor][B], cores[head->pont.cor][ALPA]);
              SDL_RenderPresent(g_pRenderer);
              SDL_Delay(10);
 
